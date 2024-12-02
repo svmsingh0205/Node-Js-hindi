751122ab4f78724dfa71eea52ffd8fec3aa863c3
svmsingh <133512575+svmsingh0205@users.noreply.github.com>
2024-12-02 14:48:12 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using nodeJS not Browser
+
+console.log(3+3)
+console.log("svm")
+
+
+let name="Shivam"
+let age=18
+let isLoggedin=false
+let state=null
+
+//number 2 to power 53
+//bigint for bigger numbers
+//string => ""
+//boolean => true/flase
+//null =>it is an standalone value and also a data Type
+//undefined => not defined 
+//symbo => mainly uses in react for find uniqueness
+
+
+//object 
+
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
