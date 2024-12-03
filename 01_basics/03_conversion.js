c3b89c87f173f1ee63c5bb13ca542f91d14b2e66
svmsingh <133512575+svmsingh0205@users.noreply.github.com>
2024-12-03 13:39:57 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,27 @@
+let score="hit"
+
+console.log(typeof score);
+console.log(typeof score);
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"=> 33
+//"33abs" => NaN
+// true => 1; false =>0
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+//1=> true; 0=> true
+// " "=> false 
+//"svm"=> true
+
+
+//conversion for string
+
+let someNum=33
+let stringNumber =String(someNum)
+console.log(stringNumber)
+console.log(typeof stringNumber)
